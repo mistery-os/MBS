30887b85f6a4ffefff34a0b19557d52a74a7f2f5
Yongseob LEE <tel.05056622231@gmail.com>
2018-03-05 14:44:59 +0900

    get_pfn_range_for_nid_pram

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5692,6 +5692,23 @@ void __meminit get_pfn_range_for_nid(unsigned int nid,
 		*start_pfn = 0;
 }
 
+void __meminit get_pfn_range_for_nid_pram(unsigned int nid,
+			unsigned long *start_pfn, unsigned long *end_pfn)
+{
+	unsigned long this_start_pfn, this_end_pfn;
+	int i;
+
+	*start_pfn = -1UL;
+	*end_pfn = 0;
+
+	for_each_pram_pfn_range(i, nid, &this_start_pfn, &this_end_pfn, NULL) {
+		*start_pfn = min(*start_pfn, this_start_pfn);
+		*end_pfn = max(*end_pfn, this_end_pfn);
+	}
+
+	if (*start_pfn == -1UL)
+		*start_pfn = 0;
+}
 /*
  * This finds a zone that can be used for ZONE_MOVABLE pages. The
  * assumption is made that zones within a node are ordered in monotonic
