5351571b4a5a1b14c86b6a5c87ab25369350d2b8
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-06-19 13:21:52 +0900

    pr_info(">>>>GOTCHA\n");

--- mm/memblock.c
+++ mm/memblock.c
@@ -1525,6 +1525,7 @@ void __init __memblock_free_early(phys_addr_t base, phys_addr_t size)
 	memblock_dbg("%s: [%#016llx-%#016llx] %pF\n",
 			__func__, (u64)base, (u64)base + size - 1,
 			(void *)_RET_IP_);
+	pr_info(">>>>GOTCHA\n");
 	kmemleak_free_part_phys(base, size);
 	memblock_remove_range(&memblock.reserved, base, size);
 }
