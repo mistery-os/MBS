586c4c7d5ee33172c7e1f8961ef94a0b80dfba97
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-08-08 17:06:10 +0900

    test87

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5942,6 +5942,8 @@ void __init sparse_memory_present_with_active_regions(int nid)
 
 	for_each_mem_pfn_range(i, nid, &start_pfn, &end_pfn, &this_nid)
 		memory_present(this_nid, start_pfn, end_pfn);
+	for_each_pram_pfn_range(i, nid, &start_pfn, &end_pfn, &this_nid)
+		memory_present(this_nid, start_pfn, end_pfn);
 #	if 0 //ifndef YONGSEOB-MBS
 	int ix;
 	for_each_pfn_range(i, ix, nid, &start_pfn, &end_pfn, &this_nid)
@@ -5950,8 +5952,6 @@ void __init sparse_memory_present_with_active_regions(int nid)
 	unsigned long start_pfn_pram, end_pfn_pram;
 	int i_pram, this_nid_pram;
 	//>>>
-	for_each_pram_pfn_range(i, nid, &start_pfn, &end_pfn, &this_nid)
-		memory_present(this_nid, start_pfn, end_pfn);
 	//>>>
 #	endif
 }
