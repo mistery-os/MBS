dac5a87754d30dd6e8f0502fdd7f58c809c301f6
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-08-09 16:55:11 +0900

    test76

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5941,15 +5941,16 @@ void __init sparse_memory_present_with_active_regions(int nid)
 	int i, this_nid;
 	for_each_mem_pfn_range(i, nid, &start_pfn, &end_pfn, &this_nid)
 		memory_present(this_nid, start_pfn, end_pfn);
+
+	unsigned long start_pfn_pram, end_pfn_pram;
+	int i_pram, this_nid_pram;
+	for_each_pram_pfn_range(i_pram, nid, &start_pfn_pram, &end_pfn_pram, &this_nid_pram)
+		memory_present(this_nid_pram, start_pfn_pram, end_pfn_pram);
 #	if 0 //ifndef YONGSEOB-MBS
 	int ix;
 	for_each_pfn_range(i, ix, nid, &start_pfn, &end_pfn, &this_nid)
 		memory_present(this_nid, start_pfn, end_pfn);
 
-	//for_each_pram_pfn_range(i_pram, nid, &start_pfn_pram, &end_pfn_pram, &this_nid_pram)
-		//memory_present(this_nid_pram, start_pfn_pram, end_pfn_pram);
-	unsigned long start_pfn_pram, end_pfn_pram;
-	int i_pram, this_nid_pram;
 	for_each_pram_pfn_range(i, nid, &start_pfn, &end_pfn, &this_nid)
 		memory_present(this_nid, start_pfn, end_pfn);
 	//<<<2018.02.14 Yongseob
