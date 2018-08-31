ea01398e99c831969934daf8014394b531b0fdf5
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-08-31 21:29:54 +0900

    test37

--- arch/x86/mm/numa_internal.h
+++ arch/x86/mm/numa_internal.h
@@ -18,6 +18,7 @@ struct numa_meminfo {
 
 void __init numa_remove_memblk_from(int idx, struct numa_meminfo *mi);
 int __init numa_cleanup_meminfo(struct numa_meminfo *mi);
+int __init numa_cleanup_meminfo_pram(struct numa_meminfo *mi);
 void __init numa_reset_distance(void);
 
 void __init x86_numa_init(void);
