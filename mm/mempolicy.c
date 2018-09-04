5f92283f93372b0fa6e100c186a96f2b6c2857c8
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-09-04 20:30:45 +0900

    test25

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -265,6 +265,41 @@ static int mpol_set_nodemask(struct mempolicy *pol,
 		ret = mpol_ops[pol->mode].create(pol, NULL);
 	return ret;
 }
+static int mpol_set_nodemask_pram(struct mempolicy *pol,
+		     const nodemask_t *nodes, struct nodemask_scratch *nsc)
+{
+	int ret;
+
+	/* if mode is MPOL_DEFAULT, pol is NULL. This is right. */
+	if (pol == NULL)
+		return 0;
+	/* Check N_MEMORY */
+	nodes_and(nsc->mask1,
+		  cpuset_current_mems_allowed, node_states[N_MEMORY]);
+
+	VM_BUG_ON(!nodes);
+	if (pol->mode == MPOL_PREFERRED && nodes_empty(*nodes))
+		nodes = NULL;	/* explicit local allocation */
+	else {
+		if (pol->flags & MPOL_F_RELATIVE_NODES)
+			mpol_relative_nodemask(&nsc->mask2, nodes, &nsc->mask1);
+		else
+			nodes_and(nsc->mask2, *nodes, nsc->mask1);
+
+		if (mpol_store_user_nodemask(pol))
+			pol->w.user_nodemask = *nodes;
+		else
+			pol->w.cpuset_mems_allowed =
+						cpuset_current_mems_allowed;
+	}
+
+	if (nodes)
+		ret = mpol_ops[pol->mode].create(pol, &nsc->mask2);
+	else
+		ret = mpol_ops[pol->mode].create(pol, NULL);
+	return ret;
+}
+
 
 /*
  * This function just creates a new policy, does some check and simple
@@ -905,7 +940,7 @@ static long do_set_prampolicy(unsigned short mode, unsigned short flags,
 		goto out;
 	}
 	old = current->prampolicy;
-	current->mbspolicy = new;
+	current->prampolicy = new;
 	if (new && new->mode == MPOL_INTERLEAVE)
 		current->il_prev = MAX_NUMNODES-1;
 	task_unlock(current);
