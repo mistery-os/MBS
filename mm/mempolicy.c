eb7a76b1027e8b1fb5407b000b1eedccbcf4d5f5
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-09-13 14:39:39 +0900

    test15

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -3321,7 +3321,7 @@ void __init numa_policy_init(void)
 			.mode = MPOL_LOCAL,
 			//.mode = MPOL_INTERLEAVE,
 			//.flags = MPOL_F_LOCAL| MPOL_F_MOF | MPOL_F_MORON,
-			. v = { .nodes = ,},
+			//. v = { .nodes = ,},
 		};
 		//>>>
 	}
