f933d7321366f43b4d0253a608f9a9df297e7022
李溶燮(Lee, Yongseob) <Yongseob.Rhee@gmail.com>
2018-11-28 16:11:48 +0900

    alloc_prams_vmalloc

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -1833,6 +1833,10 @@ EXPORT_SYMBOL(vmap);
 static void *__vmalloc_node_pram(unsigned long size, unsigned long align,
 			    gfp_t gfp_mask, pgprot_t prot,
 			    int node, const void *caller);
+static void *__vmalloc_node(unsigned long size, unsigned long align,
+			    gfp_t gfp_mask, pgprot_t prot,
+			    int node, const void *caller);
+
 static void *__vmalloc_area_node_pram(struct vm_struct *area, gfp_t gfp_mask,
 				 pgprot_t prot, int node)
 {
@@ -1888,9 +1892,6 @@ fail:
 	vfree(area->addr);
 	return NULL;
 }
-static void *__vmalloc_node(unsigned long size, unsigned long align,
-			    gfp_t gfp_mask, pgprot_t prot,
-			    int node, const void *caller);
 static void *__vmalloc_area_node(struct vm_struct *area, gfp_t gfp_mask,
 				 pgprot_t prot, int node)
 {
